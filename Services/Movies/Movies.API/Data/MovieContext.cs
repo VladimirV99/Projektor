eb0998c4d64e370cbbe05fe1df55702023d7eaf2
Aleska Kojadinovic <kojadinovic.aleksa98@gmail.com>
2022-03-18 20:07:40 +0100

    run pending migrations on startup

--- Services/Movies/Movies.API/Data/MovieContext.cs
+++ Services/Movies/Movies.API/Data/MovieContext.cs
@@ -1,19 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Movies.API.Entities;
 
-namespace Movies.API.Data {
-    public class MovieContext : DbContext {
+namespace Movies.API.Data
+{
+    public class MovieContext : DbContext
+    {
         public DbSet<Movie> Movies { get; }
         public DbSet<Person> People { get; }
         public DbSet<MoviePerson> MoviePeople { get; }
 
-        public MovieContext(DbContextOptions options) : base(options) {
-
+        public MovieContext(DbContextOptions options) : base(options)
+        {
         }
 
-        protected override void OnModelCreating(ModelBuilder builder) {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
             base.OnModelCreating(builder);
-            builder.Entity<MoviePerson>().HasKey(person => new { person.MovieId, person.PersonId, person.RoleId });
+            builder.Entity<MoviePerson>().HasKey(person => new {person.MovieId, person.PersonId, person.RoleId});
         }
     }
 }
--- Services/Movies/Movies.API/Program.cs
+++ Services/Movies/Movies.API/Program.cs
@@ -24,4 +24,15 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<MovieContext>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
+
 app.Run();

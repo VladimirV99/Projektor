3e9cbde51d1bbc3e8cd970843434f23597e6b916
Vladimir Vuksanovic <vladevuksan99@gmail.com>
2022-03-19 15:00:53 +0100

    Add authorization support to swagger

--- Security/Identity/Program.cs
+++ Security/Identity/Program.cs
@@ -1,5 +1,7 @@
 using Identity.Extensions;
 using Identity.Services;
+using Identity.Swagger;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +19,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Identity Server",
+        Description = "Identity Server for Projektor Application"
+    });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter JWT access token:",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    options.OperationFilter<AuthOperationFilter>();
+});
 
 var app = builder.Build();
 
---
+++ Security/Identity/Swagger/AuthOperationFilter.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Text;
+
+namespace Identity.Swagger
+{
+    public class AuthOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var anonymousAttribute = context.MethodInfo
+                .GetCustomAttributes(true)
+                .OfType<AllowAnonymousAttribute>();
+            if (anonymousAttribute.Any())
+                return;
+
+            var classAuthAttributes = context.MethodInfo.DeclaringType!
+                .GetCustomAttributes(true)
+                .OfType<AuthorizeAttribute>();
+
+            var methodAuthAttributes = context.MethodInfo
+                .GetCustomAttributes(true)
+                .OfType<AuthorizeAttribute>();
+
+            var authAttributes = classAuthAttributes.Concat(methodAuthAttributes).Distinct();
+            if (authAttributes.Any())
+            {
+                var requiredRoles = authAttributes
+                    .Where(attribute => attribute.Roles != null)
+                    .Select(attribute => string.Join(" / ", attribute.Roles!.Split(",").Select(role => role.Trim())))
+                    .DefaultIfEmpty("Any");
+
+                StringBuilder sb = new();
+                sb.Append("<b>Required Access Privileges</b>: ");
+                sb.Append(string.Join(", ", requiredRoles));
+                operation.Description += sb.ToString();
+
+                operation.Responses.TryAdd("401", new OpenApiResponse { Description = "Unauthorized" });
+                operation.Responses.TryAdd("403", new OpenApiResponse { Description = "Forbidden" });
+
+                var jwtBearerScheme = new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                };
+
+                operation.Security = new List<OpenApiSecurityRequirement>
+                {
+                    new OpenApiSecurityRequirement
+                    {
+                        [ jwtBearerScheme ] = Array.Empty<string>()
+                    }
+                };
+            }
+        }
+    }
+}

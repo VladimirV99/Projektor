21d60c64d9549796d171538c5a2eabf8b4fe5e9f
aleksa <kojadinovic.aleksa98@gmail.com>
2022-03-22 19:29:05 +0100

    start writing seed script

---
+++ Services/Movies/Movies.API/Seed/seed_script.sql
@@ -0,0 +1,20 @@
+﻿GO
+CREATE VIEW movies_tmp AS
+SELECT Id, Title, Year, Length, ImdbUrl FROM Movies;
+
+GO
+DELETE FROM Movies;
+
+GO
+BULK INSERT movies_tmp
+FROM '/var/seed/movies.csv'
+WITH (FORMAT = 'CSV'
+      , FIRSTROW=2
+      , ROWTERMINATOR = '0x0a');
+
+GO 
+DROP VIEW movies_tmp;
+
+
+
+
---
+++ Services/Movies/Movies.API/dbseed.sh
@@ -0,0 +1,1 @@
+﻿docker exec -it movies.db /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'MatfRs2_Movies' -d MoviesDB -i /var/seed/seed_script.sql

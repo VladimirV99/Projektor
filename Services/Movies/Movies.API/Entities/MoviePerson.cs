dcac07449c81f4eba121073dc7da43f0af6bb8a8
aleksa <kojadinovic.aleksa98@gmail.com>
2022-03-24 16:37:31 +0100

    CR fix

--- Services/Movies/Movies.API/Entities/MoviePerson.cs
+++ Services/Movies/Movies.API/Entities/MoviePerson.cs
@@ -7,14 +7,14 @@ namespace Movies.API.Entities
     {
         [Required(ErrorMessage = ErrorMessages.MOVIE_ID_REQUIRED)]
         public int MovieId { get; set; }
-        public Movie? Movie { get; set; }
+        public Movie Movie { get; set; }
 
         [Required(ErrorMessage = ErrorMessages.ROLE_ID_REQUIRED)]
         public int RoleId { get; set; }
-        public Role? Role { get; set; }
+        public Role Role { get; set; }
 
         [Required(ErrorMessage = ErrorMessages.PERSON_ID_REQUIRED)]
         public int PersonId { get; set; }
-        public Person? Person { get; set; }
+        public Person Person { get; set; }
     }
 }
--- Services/Movies/Movies.API/dbseed.sh
+++
@@ -1,1 +0,0 @@
-﻿docker exec -it movies.db /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'MatfRs2_Movies' -d MoviesDB -i /var/seed/seed_script.sql

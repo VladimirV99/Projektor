49897e14cba688e91016d6f58e43d98266872bfd
aleksa <kojadinovic.aleksa98@gmail.com>
2022-04-14 19:44:40 +0200

    CR again

--- Services/Movies/Movies.API/Controllers/MoviesController.cs
+++ Services/Movies/Movies.API/Controllers/MoviesController.cs
@@ -23,35 +23,35 @@ namespace Movies.API.Controllers
         public async Task<IActionResult> GetMovieById(int id)
         {
             var movie = await _repository.GetMovieById(id);
-            return movie == null ? NotFound() : Ok(_mapper.Map<Movie, MovieModel>(movie));
+            return movie == null ? NotFound() : Ok(_mapper.Map<MovieModel>(movie));
         }
 
         [HttpGet("[action]/{id}")]
         public async Task<IActionResult> GetMoviesByGenre(int id)
         {
             var movies = await _repository.GetMoviesByGenreId(id);
-            return Ok(_mapper.Map<List<Movie>, List<MovieModel>>(movies));
+            return Ok(_mapper.Map<List<MovieModel>>(movies));
         }
 
         [HttpGet("[action]/{id}")]
         public async Task<IActionResult> GetMoviesByPerson(int id)
         {
             var movies = await _repository.GetMoviesByPerson(id);
-            return Ok(_mapper.Map<List<Movie>, List<MovieModel>>(movies));
+            return Ok(_mapper.Map<List<MovieModel>>(movies));
         }
         
         [HttpGet("[action]")]
         public async Task<IActionResult> FilterMovies([FromQuery] FilterMoviesRequest request)
         {
             var (movies, count) = await _repository.FilterMovies(request);
-            return Ok(new PaginatedMovieList {Movies = _mapper.Map<List<Movie>, List<MovieModel>>(movies), Count = count});
+            return Ok(new PaginatedMovieList {Movies = _mapper.Map<List<MovieModel>>(movies), Count = count});
         }
         
         [HttpGet("[action]")]
         public async Task<IActionResult> GetGenres()
         {
             var genres = await _repository.GetGenres();
-            return Ok(_mapper.Map<List<Genre>, List<GenreSimpleModel>>(genres));
+            return Ok(_mapper.Map<List<GenreSimpleModel>>(genres));
         }
 
         [HttpGet("[action]")]

7881a77ed1efe6ab9c9156e95b66d64aae861387
aleksa <kojadinovic.aleksa98@gmail.com>
2022-04-25 14:25:58 +0200

    revert test

--- app/src/features/HomeScreen/index.tsx
+++ app/src/features/HomeScreen/index.tsx
@@ -1,26 +1,5 @@
-import axiosAuthInstance from 'axios/instance';
-
 const HomeScreen = () => {
-    return (
-        <div>
-            <button
-                onClick={() => {
-                    axiosAuthInstance
-                        .get(
-                            'http://localhost:7060/api/v1/Administrator/GetAllUsers'
-                        )
-                        .then(() => {
-                            console.log('success');
-                        })
-                        .catch(() => {
-                            console.log('error');
-                        });
-                }}
-            >
-                Hello
-            </button>
-        </div>
-    );
+    return <div>under construction</div>;
 };
 
 export default HomeScreen;

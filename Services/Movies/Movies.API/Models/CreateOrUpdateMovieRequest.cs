c25e99446e3018eba898b3d0b269860f923253b0
aleksa <kojadinovic.aleksa98@gmail.com>
2022-04-27 18:07:56 +0200

    Add separate model for create/update

---
+++ Services/Movies/Movies.API/Models/CreateOrUpdateMovieRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Movies.API.Models;
+
+public class CreateOrUpdateMovieRequest
+{
+    public int? Id { get; set; }
+    public string Title { get; set; }
+    public int Length { get; set; }
+    public uint Year { get; set; }
+    public string? TrailerUrl { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? ImdbUrl { get; set; }
+    public List<int> Genres { get; set; }
+    public List<PersonRole> People { get; set; }
+}
---
+++ Services/Movies/Movies.API/Models/PersonRole.cs
@@ -0,0 +1,6 @@
+﻿namespace Movies.API.Models;
+
+public class PersonRole
+{
+    
+}

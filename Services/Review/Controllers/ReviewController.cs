23551def1882abe70113d57d0e46d2d1d875ad4a
Vladimir Vuksanovic <vladevuksan99@gmail.com>
2022-04-28 22:41:32 +0200

    Add review controller skeleton

---
+++ Services/Review/Controllers/ReviewController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Review.Models;
+
+namespace Review.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReviewController : ControllerBase
+    {
+        // This is a debug function
+        // Watched movies should be added asynchronously by the reservation service
+        [HttpPost("[action]")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> AddWatchedMovie([FromBody] AddWatchedMovieRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        // [Authorize(Roles = Role.CUSTOMER)]
+        [HttpPost("[action]")]
+        // TODO Return 201
+        [ProducesResponseType(typeof(MovieReviewModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<MovieReviewModel>> CreateReview([FromBody] CreateReviewRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("[action]/{reviewId}")]
+        [ProducesResponseType(typeof(MovieReviewModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<MovieReviewModel>> GetReview(string reviewId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("[action]")]
+        [ProducesResponseType(typeof(IEnumerable<MovieReviewModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<MovieReviewModel>>> GetReviewsForMovie([FromQuery] GetReviewsForMovieRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/Review/Models/AddWatchedMovieRequest.cs
@@ -0,0 +1,17 @@
+﻿using Review.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace Review.Models
+{
+    public class AddWatchedMovieRequest
+    {
+        [Required(ErrorMessage = ErrorMessages.MOVIE_ID_REQUIRED)]
+        public int MovieId { get; set; }
+
+        [Required(ErrorMessage = ErrorMessages.USER_ID_REQUIRED)]
+        public string UserId { get; set; } = null!;
+
+        [Required(ErrorMessage = ErrorMessages.WATCHED_TIME_REQUIRED)]
+        public DateTime WatchedOn { get; set; }
+    }
+}

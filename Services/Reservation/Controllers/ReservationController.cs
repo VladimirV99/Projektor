5135720d8c22fc5815acaf39ecedc5378888410d
Lazar Čeliković <celikoviclazar@hotmail.com>
2022-05-22 14:44:02 +0200

    Add controller for reservation service

---
+++ Services/Reservation/Controllers/ReservationController.cs
@@ -0,0 +1,17 @@
+﻿using Reservation.Data;
+using AutoMapper;
+
+namespace Reservation.Controllers
+{
+    public class ReservationController
+    {
+        private readonly IReservationRepository _repository;
+        private readonly IMapper _mapper;
+
+        public ReservationController(IReservationRepository repository, IMapper mapper)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+    }
+}

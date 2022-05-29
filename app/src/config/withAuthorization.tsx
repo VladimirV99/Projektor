c39e11fc1bb55da418fc4772be0042cd0ae4a023
aleksa <kojadinovic.aleksa98@gmail.com>
2022-05-29 22:41:08 +0200

    Refactor withAuthorization to use util function

--- app/src/config/withAuthorization.tsx
+++ app/src/config/withAuthorization.tsx
@@ -2,6 +2,7 @@ import { access } from 'fs/promises';
 import { Fragment, useEffect, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import { isUserAdmin } from 'util/auth';
 
 type Props = {
     children: JSX.Element | JSX.Element[];
@@ -9,26 +10,9 @@ type Props = {
 };
 
 export default function WithAuthorization({ role, children }: Props) {
-    const dispatch = useDispatch();
-
-    const accessToken = useMemo(
-        () => window.localStorage.getItem('accessToken'),
-        []
+    return isUserAdmin() ? (
+        <Fragment>{children}</Fragment>
+    ) : (
+        <Navigate to="/login" />
     );
-    if (!accessToken) {
-        return <Navigate to="/not-found" />;
-    }
-
-    const rolesEncoded = JSON.parse(window.atob(accessToken.split('.')[1]))[
-        'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'
-    ];
-
-    if (
-        (Array.isArray(rolesEncoded) && !rolesEncoded.includes(role)) ||
-        rolesEncoded !== role
-    ) {
-        return <Navigate to="/not-found" />;
-    }
-
-    return <Fragment>{children}</Fragment>;
 }

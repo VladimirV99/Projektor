12bdec0a18e1017b26be243f8ec2835343f37e64
Vladimir Vuksanovic <vladevuksan99@gmail.com>
2022-05-30 14:42:41 +0200

    Add reservation halls and seats to seeder

---
+++ Tools/Seeder/Seeders/ReservationSeeder.cs
@@ -0,0 +1,41 @@
+using System.Data;
+using Seeder.Util;
+
+namespace Seeder.Seeders;
+
+public static class ReservationSeeder
+{
+    public static void Seed()
+    {
+        const string hallsDataPath            = "Data/Reservations/halls.tsv";
+        const string seatsDataPath            = "Data/Reservations/seats.tsv";
+        // const string reservationsDataPath     = "Data/Reservations/reservations.tsv";
+        // const string reservationSeatsDataPath = "Data/Reservations/reservation_seats.tsv";
+        
+        try
+        {
+            // Read data files
+            DataTable dtHalls            = CsvReader.ReadCsv(hallsDataPath) ?? throw new NullReferenceException(nameof(dtHalls));
+            DataTable dtSeats            = CsvReader.ReadCsv(seatsDataPath) ?? throw new NullReferenceException(nameof(dtSeats));
+            // DataTable dtReservations     = CsvReader.ReadCsv(reservationsDataPath) ?? throw new NullReferenceException(nameof(dtReservations));
+            // DataTable dtReservationSeats = CsvReader.ReadCsv(reservationSeatsDataPath) ?? throw new NullReferenceException(nameof(dtReservationSeats));
+            
+            TableInsertRecord[] seedTables = {
+                new ("Halls", dtHalls, true),
+                new ("Seats", dtSeats, false)
+                // new ("Reservations", dtReservations, true),
+                // new ("ReservationSeats", dtReservationSeats, false)
+            };
+            
+            SqlHelper.SeedData(
+                "Server=localhost;Database=ReservationDB;User Id=sa;Password=MatfRs2_MSSQL;",
+                seedTables
+            );
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}

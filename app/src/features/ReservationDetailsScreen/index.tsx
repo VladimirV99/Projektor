581a233baabc547953e9048c2417e88c8cbf0be7
Lazar Čeliković <celikoviclazar@hotmail.com>
2022-07-02 19:43:17 +0200

    Prettier

--- app/src/features/ReservationDetailsScreen/index.tsx
+++ app/src/features/ReservationDetailsScreen/index.tsx
@@ -25,7 +25,9 @@ const ReservationDetailsScreen = () => {
     const navigate = useNavigate();
     const isCustomer = isUserCustomer();
 
-    const [screening, setScreening] = useState<Screening | null | undefined>(undefined);
+    const [screening, setScreening] = useState<Screening | null | undefined>(
+        undefined
+    );
     const [seats, setSeats] = useState<SeatModel[][] | null>(null);
     const [isModalOpened, setIsModalOpened] = useState<boolean>(false);
     const [selectedSeatMatrix, setSelectedSeatMatrix] = useState<
@@ -35,7 +37,7 @@ const ReservationDetailsScreen = () => {
     const [numberOfSelectedSeats, setNumberOfSelectedSeats] =
         useState<number>(0);
 
-    if(screening === null) return <SomethingWentWrong />
+    if (screening === null) return <SomethingWentWrong />;
 
     const currentUser = useSelector(selectIsUserLoggedIn);
 
@@ -176,7 +178,9 @@ const ReservationDetailsScreen = () => {
                 </div>
                 {!currentUser && (
                     <div>
-                        <h3 style={{ color: 'red' }}>You must be logged in to create reservations</h3>
+                        <h3 style={{ color: 'red' }}>
+                            You must be logged in to create reservations
+                        </h3>
                     </div>
                 )}
                 {currentUser && (
